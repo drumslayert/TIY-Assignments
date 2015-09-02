402f1e221c262e029d8603d12759d78058ac0e66
drumslayert <ddc@terrymcalhoun.com>
2015-09-01 20:02:52 -0400

    my halfway basic calculator

---
+++ BasicCalculator.js
@@ -0,0 +1,33 @@
+console.assert(add(0, 0) === 0);
+console.assert(add(0, 1) === 1);
+console.assert(add(0, 2) === 2);
+console.assert(add(0, 3) === 3);
+console.assert(add(0, 4) === 4);
+console.assert(add(0, 5) === 5);
+console.assert(add(0, 6) === 6);
+console.assert(add(0, 7) === 7);
+console.assert(add(0, 8) === 8);
+console.assert(add(0, 9) === 9);
+
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+function add(A, B){
+  return A + B;
+}
+console.assert(sub(0, 0) === 0);
+console.assert(sub(0, 1) === -1);
+console.assert(sub(0, 2) === -2);
+console.assert(sub(0, 3) === -3);
+console.assert(sub(0, 4) === -4);
+console.assert(sub(0, 5) === -5);
+console.assert(sub(0, 6) === -6);
+console.assert(sub(0, 7) === -7);
+console.assert(sub(0, 8) === -8);
+console.assert(sub(0, 9) === -9);
+
+function sub(A, B){
+  return A - B;
+}
