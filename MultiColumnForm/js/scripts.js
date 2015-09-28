c3010addf570df054fa1a53a3ca8ab60daa74d02
drumslayert <ddc@terrymcalhoun.com>
2015-09-28 00:04:55 -0400

    update multicolumnform jquery attempt

---
+++ MultiColumnForm/js/scripts.js
@@ -0,0 +1,25 @@
+// function form(){
+// //var input = document.getElementById('input').value;
+// var input = document.querySelector('input').value;
+// var output = input.value;
+// console.log(output)
+// }
+
+// var textarea = document.querySelector("input");
+// var from = textarea.addEventListener("keydown", function(){
+//     console.log(from)
+// });
+
+/*
+alert("is this working");//It works so I know it's linked up
+console.log($.submit);  // undefined
+*/
+
+/*
+$().click(function() {
+ $("#my-form").submit();
+});
+*/
+$("#my-form").submit(function(){
+       console.log()
+  });
