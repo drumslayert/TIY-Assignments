91a9ced7ad8e29c24ef8c21391f2a3a20f199c14
drumslayert <ddc@terrymcalhoun.com>
2015-10-08 17:54:53 -0400

    added javascript and updated some sass and html

---
+++ SlideOutMenu/js/main.js
@@ -0,0 +1,7 @@
+$( ".sliders a" ).click(function() { //this sets a click event on the nav links
+  // console.log("clicked on link");
+$(this).addClass("active"); //wherever this click happens apply the class of active
+// console.log($(this).attr("href"));
+var sectionId = $(this).attr("href"); //give me the value of this href
+$(sectionId).slideToggle("active").siblings().removeClass("active");
+}); //now toggle the class of active on that value and also removeClass active of its siblings
