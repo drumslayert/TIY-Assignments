9828256a77dc472a40b75558b0ac52520c1a0ba3
drumslayert <ddc@terrymcalhoun.com>
2015-10-10 17:51:47 -0400

    fixed opacity on hover and a few tweaks

--- SlideOutMenu/js/main.js
+++ SlideOutMenu/js/main.js
@@ -1,7 +1,11 @@
 $( ".sliders a" ).click(function() { //this sets a click event on the nav links
-  // console.log("clicked on link");
+// console.log("clicked on link");
 $(this).addClass("active"); //wherever this click happens apply the class of active
 // console.log($(this).attr("href"));
- //give me the value of this href
+// give me the value of this href
 $($(this).attr("href")).toggleClass("active").siblings().removeClass("active");
 }); //now toggle the class of active on that value and also removeClass active of its siblings
+
+// <i class="fa fa-sort-asc"></i>
+//  $(this).addClass("fa fa-sort-asc");
+// .removeClass("fa fa-sort-desc")
