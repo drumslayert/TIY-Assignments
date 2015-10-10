591db5700890e68b925ba6ff21d275c39d3c2632
drumslayert <ddc@terrymcalhoun.com>
2015-10-10 12:23:07 -0400

    trying to figure out fixes but nothing is really changed yet

---
+++ ResponsiveGrid/js/main.js
@@ -0,0 +1,9 @@
+
+// $(document).ready(function(){
+//    $('div').mouseover(function(){
+//       $(this).addClass('hover');
+//    });
+//    $('div').mouseout(function(){
+//       $(this).removeClass('hover');
+//    });
+// });
