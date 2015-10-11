2262f308a61e7dc53a5b794811acbf3b94e772dc
drumslayert <ddc@terrymcalhoun.com>
2015-10-10 21:30:36 -0400

    tweaks to everything

--- SlideOutMenu/js/main.js
+++ SlideOutMenu/js/main.js
@@ -3,7 +3,7 @@ $( ".sliders a" ).click(function() { //this sets a click event on the nav links
 $(this).addClass("active"); //wherever this click happens apply the class of active
 // console.log($(this).attr("href"));
 // give me the value of this href
-$($(this).attr("href")).toggleClass("active").siblings().removeClass("active");
+$($(this).attr("href")).slideToggle("active").siblings().slideUp("active");
 }); //now toggle the class of active on that value and also removeClass active of its siblings
 
 // <i class="fa fa-sort-asc"></i>
